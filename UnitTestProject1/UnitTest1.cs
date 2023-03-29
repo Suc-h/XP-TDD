9c98081d17230aa62ba144ac13adbf2469e267d1
Suc-h <97514706+Suc-h@users.noreply.github.com>
2023-03-29 08:24:41 +0200

    Nahraní ?

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1POS()
+        {
+            string name = unitTestSeSuchem.Form1.GetName("Martin");
+            Assert.IsTrue(name.Length < 10);
+
+        }
+
+        [TestMethod]
+        public void TestMethod1NEG()
+        {
+            string name = unitTestSeSuchem.Form1.GetName("David Suchy");
+            Assert.IsTrue(name.Length < 10);
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod2POS()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetSpecialziation("Kouzelník");
+            Assert.IsTrue(truefalse);
+         
+        }
+
+        [TestMethod]
+        public void TestMethod2NEG()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetSpecialziation("Rampelník");
+            Assert.IsTrue(truefalse);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod3POS()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetFace("velký nos");
+            Assert.IsTrue(truefalse);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3NEG()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetFace("klavesnice");
+            Assert.IsTrue(truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod4POS()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetHair("pleška");
+            Assert.IsTrue(truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod4NEG()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetHair("dildo");
+            Assert.IsTrue(truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod5POS()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetHairColour("blond");
+            Assert.IsTrue(truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod5NEG()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetHairColour("zrzek");
+            Assert.IsTrue(truefalse);
+
+        }
+
+        [TestMethod]
+        public void TestMethod6POS()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetJob("obchodník");
+            Assert.IsTrue(truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod6NEG()
+        {
+            bool truefalse = unitTestSeSuchem.Form1.GetJob("koček");
+            Assert.IsTrue(truefalse);
+
+        }
+
+        [TestMethod]
+        public void TestMethod7POS()
+        {
+            string truefalse = unitTestSeSuchem.Form1.GetBoss("Ano");
+            Assert.AreSame("Ano", truefalse);
+
+        }
+        [TestMethod]
+        public void TestMethod7NEG()
+        {
+            string truefalse = unitTestSeSuchem.Form1.GetBoss("Ne");
+            Assert.AreSame("Ano", truefalse);
+        }
+
+        [TestMethod]
+        public void TestMethod8POS()
+        {
+            int truefalse = unitTestSeSuchem.Form1.GetX(69);
+            Assert.IsTrue(truefalse > 0 && truefalse < 100);
+
+        }
+        [TestMethod]
+        public void TestMethod8NEG()
+        {
+            int truefalse = unitTestSeSuchem.Form1.GetX(150);
+            Assert.IsTrue(truefalse > 0 && truefalse < 100);
+        }
+
+        [TestMethod]
+        public void TestMethod9POS()
+        {
+            int truefalse = unitTestSeSuchem.Form1.GetY(18);
+            Assert.IsTrue(truefalse > 0 && truefalse < 100);
+
+        }
+        [TestMethod]
+        public void TestMethod9NEG()
+        {
+            int truefalse = unitTestSeSuchem.Form1.GetY(-9);
+            Assert.IsTrue(truefalse > 0 && truefalse < 100);
+        }
+
+       
+
+        [TestMethod]
+        public void TestMethod10POS()
+        {
+            
+            int xp = unitTestSeSuchem.Form1.GetXP(1);
+            int level = unitTestSeSuchem.Form1.GetLevel(18);
+            Assert.IsTrue(xp * level > 0 && xp*level < 100);
+
+        }
+        [TestMethod]
+        public void TestMethod10NEG()
+        {
+            int xp = unitTestSeSuchem.Form1.GetXP(10);
+            int level = unitTestSeSuchem.Form1.GetLevel(18);
+            Assert.IsTrue(xp * level > 0 && xp * level < 100);
+        }
+
+
+    }
+}
---
+++ unitTestSeSuchem/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace unitTestSeSuchem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public static string jmeno = "";
+        public static string specializace = "";
+
+        public static string GetName(string jmeno)
+        {
+            return jmeno; 
+        }
+
+        public static bool GetSpecialziation(string specializace)
+        {
+            string[] pole = { "Kouzelník", "Berserker", "Inženýr", "Cizák" };
+            if (pole.Contains(specializace)) return true;
+            return false;
+        }
+        public static bool GetFace(string face)
+        {
+            string[] pole = { "velký nos", "ušoplesk", "make-up" };
+            if (pole.Contains(face)) return true;
+            return false;
+        }
+        public static bool GetHair(string hair)
+        {
+            string[] pole = { "drdol", "culík", "pleška" };
+            if (pole.Contains(hair)) return true;
+            return false;
+        }
+        public static bool GetHairColour(string colour)
+        {
+            string[] pole = { "kaštanová", "blond", "červená" };
+            if (pole.Contains(colour)) return true;
+            return false;
+        }
+
+        public static bool GetJob(string job)
+        {
+            string[] pole = { "obchodník", "nepřítel", "obyvatel" };
+            if (pole.Contains(job)) return true;
+            return false;
+        }
+
+        public static string GetBoss(string jeboss)
+        {            
+            return jeboss;
+        }
+        public static int GetX(int x)
+        {
+            return x;
+        }
+        public static int GetY(int y)
+        {
+            return y;
+        }
+       
+        public static int GetLevel(int level)
+        {
+            return level;
+        }
+        public static int GetXP(int xp)
+        {
+            return xp;
+        }
+
+    }
+}
